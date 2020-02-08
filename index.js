9c37348b1e7859c084dc4bf0432051fbc6247c0a
antonkalik <antonkalik@gmail.com>
2020-02-08 04:18:57 +0200

    spin updated

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+const process = require('process');
+const readline = require('readline');
+const std = process.stdout;
 const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
@@ -6,7 +9,37 @@ const fileName = './package.json';
 const file = require(path.resolve(fileName));
 
 async function updatePackages({ dependencies, devDependencies }) {
-  const log = message => console.log('\x1b[32m', '\x1b[1m', message);
+  const log = message => {
+    console.log('\x1b[32m', '\x1b[1m', message);
+  };
+  const spinner = speed => {
+    let index = 0;
+    let stop = false;
+
+    readline.cursorTo(process.stdout, 0);
+    const spinners = ['-', '\\', '|', '/'];
+
+    function spin() {
+      if (!stop) {
+        setTimeout(() => {
+          readline.cursorTo(process.stdout, 0);
+          std.write(spinners[index]);
+          index++;
+          if (index + 1 === spinners.length) {
+            index = 0;
+          }
+          spin();
+        }, speed);
+      }
+    }
+    return {
+      spin,
+      stop: () => {
+        stop = true;
+      },
+    };
+  };
+
   log('get dependencies...');
   const allDependencies = {
     dependencies: Object.keys(dependencies),
@@ -36,14 +69,19 @@ async function updatePackages({ dependencies, devDependencies }) {
   const stringOfListDevDependencies = JSON.parse(fromBuffer).devDependencies.join(' ');
 
   try {
+    spinner.spin();
     log('removing node_modules...');
     await exec('rm -rf node_modules');
     log('node_modules removed!');
     log('installing new packages...');
     await exec('npm i -S ' + stringOfListDependencies);
+    log('dependencies has been installed!');
     await exec('npm i -D ' + stringOfListDevDependencies);
+    log('devDependencies has been installed!');
     log(`Dependencies has been installed and ${fileName} has been updated!`);
+    spinner.stop();
   } catch (e) {
+    spinner.stop();
     console.error('EXEC ERROR', e);
   }
 }
