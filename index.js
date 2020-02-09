ba22e51fa09faacd103e445094e98190f4a3cceb
antonkalik <antonkalik@gmail.com>
2020-02-09 19:55:40 +0200

    prompt before update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const process = require('process');
 const readline = require('readline');
-const std = process.stdout;
+const { stdin, stdout } = process;
 const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
@@ -15,25 +15,25 @@ function Logger() {
   let white = '\x1b[37m';
   return {
     success: message => {
-      std.clearLine(0);
-      std.cursorTo(1);
-      std.write(bold);
-      std.write(green);
-      std.write('✔ ' + message + '.' + '\n');
+      stdout.clearLine(0);
+      stdout.cursorTo(1);
+      stdout.write(bold);
+      stdout.write(green);
+      stdout.write('✔ ' + message + '.' + '\n');
     },
     process: message => {
-      std.clearLine(0);
-      std.cursorTo(3);
-      std.write(bold);
-      std.write(white);
-      std.write(message + '...');
+      stdout.clearLine(0);
+      stdout.cursorTo(3);
+      stdout.write(bold);
+      stdout.write(white);
+      stdout.write(message + '...');
     },
     error: message => {
-      std.clearLine(0);
-      std.cursorTo(1);
-      std.write(bold);
-      std.write(red);
-      std.write('✘ ' + message + '.' + '\n');
+      stdout.clearLine(0);
+      stdout.cursorTo(1);
+      stdout.write(bold);
+      stdout.write(red);
+      stdout.write('✘ ' + message + '.' + '\n');
     },
   };
 }
@@ -42,20 +42,19 @@ function Spinner(speed = 100) {
   let index = 0;
   let spinners = ['-', '\\', '|', '/'];
 
-  function spin() {
-    setTimeout(() => {
-      readline.cursorTo(std, 1);
-      process.stderr.write('\x1B[?25l');
-      std.write(spinners[index]);
-      index++;
-      if (index + 1 === spinners.length) {
-        index = 0;
-      }
-      spin();
-    }, speed);
-  }
   return {
-    start: spin,
+    spin() {
+      setTimeout(() => {
+        readline.cursorTo(stdout, 1);
+        process.stderr.write('\x1B[?25l');
+        stdout.write(spinners[index]);
+        index++;
+        if (index + 1 === spinners.length) {
+          index = 0;
+        }
+        this.spin();
+      }, speed);
+    },
     stop: () => process.exit(),
   };
 }
@@ -63,75 +62,95 @@ function Spinner(speed = 100) {
 const spinner = Spinner(100);
 const log = Logger();
 
-async function updatePackages({ dependencies, devDependencies }) {
-  let allDependencies;
-  spinner.start();
-  log.process('Getting dependencies');
-  log.success('Prefetch dependencies');
-
-  try {
-    allDependencies = {
-      dependencies: Object.keys(dependencies),
-      devDependencies: Object.keys(devDependencies),
-    };
-  } catch {
-    throw 'Looks like something went wrong with your dependencies in ' + fileName;
-  }
-
-  let data = JSON.stringify(allDependencies);
-  log.success('Dependencies has been fetched');
-
-  log.process('Saving to buffer');
-  let buffer = Buffer.from(data);
-  let fileWithoutDependencies = file;
-  log.success('Dependencies has been saved to buffer');
-
-  log.process(`Removing old packages versions from ${fileName}`);
-  delete fileWithoutDependencies.dependencies;
-  delete fileWithoutDependencies.devDependencies;
-  log.success('Old packages has been removed');
-
-  log.process('Getting data from buffer');
-  let fromBuffer = buffer.toString();
-
-  log.process(`Writing data to ${fileName}`);
-  fs.writeFile(fileName, JSON.stringify(fileWithoutDependencies), e => {
-    if (e) throw `Write ${fileName} error`;
-    log.success(`${fileName} has been updated`);
+function updatePackages(file) {
+  const rl = readline.createInterface({
+    input: stdin,
+    output: stdout
   });
 
-  const stringOfListDependencies = JSON.parse(fromBuffer).dependencies.join(' ');
-  const stringOfListDevDependencies = JSON.parse(fromBuffer).devDependencies.join(' ');
+  rl.question("Ready? Press [y] if yes or [n] if no: ", res => {
+    if (res === 'y') {
+      onUpdate(file).catch(e => {
+        log.error(e);
+        spinner.stop();
+      });
+    } else {
+      rl.close();
+    }
+  });
 
-  try {
-    log.process('Removing node_modules');
-    await exec('rm -rf node_modules');
-    log.success('node_modules removed');
-  } catch {
-    throw "Can't remove node_modules";
-  }
+  rl.on("close", () => {
+    console.log("\nSee ya");
+    process.exit(0);
+  });
 
-  try {
-    log.process('Installing new packages');
-    log.process('Installing dependencies');
-    await exec('npm i -S ' + stringOfListDependencies);
-    log.success('dependencies has been installed');
-  } catch {
-    throw "Can't installing dependencies";
-  }
+  async function onUpdate({ dependencies, devDependencies }) {
+    let allDependencies;
+    spinner.spin();
+    log.process('Getting dependencies');
+    log.success('Prefetch dependencies');
 
-  try {
-    log.process('Installing devDependencies');
-    await exec('npm i -D ' + stringOfListDevDependencies);
-    log.success('devDependencies has been installed');
-    log.success(`All dependencies has been installed and ${fileName} has been updated`);
-    spinner.stop();
-  } catch (e) {
-    throw "Can't installing devDependencies";
+    try {
+      allDependencies = {
+        dependencies: Object.keys(dependencies),
+        devDependencies: Object.keys(devDependencies),
+      };
+    } catch {
+      throw 'Looks like something went wrong with your dependencies in ' + fileName;
+    }
+
+    let data = JSON.stringify(allDependencies);
+    log.success('Dependencies has been fetched');
+
+    log.process('Saving to buffer');
+    let buffer = Buffer.from(data);
+    let fileWithoutDependencies = file;
+    log.success('Dependencies has been saved to buffer');
+
+    log.process(`Removing old packages versions from ${fileName}`);
+    delete fileWithoutDependencies.dependencies;
+    delete fileWithoutDependencies.devDependencies;
+    log.success('Old packages has been removed');
+
+    log.process('Getting data from buffer');
+    let fromBuffer = buffer.toString();
+
+    log.process(`Writing data to ${fileName}`);
+    fs.writeFile(fileName, JSON.stringify(fileWithoutDependencies), e => {
+      if (e) throw `Write ${fileName} error`;
+      log.success(`${fileName} has been updated`);
+    });
+
+    const stringOfListDependencies = JSON.parse(fromBuffer).dependencies.join(' ');
+    const stringOfListDevDependencies = JSON.parse(fromBuffer).devDependencies.join(' ');
+
+    try {
+      log.process('Removing node_modules');
+      await exec('rm -rf node_modules');
+      log.success('node_modules removed');
+    } catch {
+      throw "Can't remove node_modules";
+    }
+
+    try {
+      log.process('Installing new packages');
+      log.process('Installing dependencies');
+      await exec('npm i -S ' + stringOfListDependencies);
+      log.success('dependencies has been installed');
+    } catch {
+      throw "Can't installing dependencies";
+    }
+
+    try {
+      log.process('Installing devDependencies');
+      await exec('npm i -D ' + stringOfListDevDependencies);
+      log.success('devDependencies has been installed');
+      log.success(`All dependencies has been installed and ${fileName} has been updated`);
+      spinner.stop();
+    } catch (e) {
+      throw "Can't installing devDependencies";
+    }
   }
 }
 
-updatePackages(file).catch(e => {
-  log.error(e);
-  spinner.stop();
-});
+updatePackages(file);
